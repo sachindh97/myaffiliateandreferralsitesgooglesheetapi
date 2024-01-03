d2501e4b7f0221869a11dddfddb56e403db48191
sachin dhabale <sachindhabale81@gmail.com>
2024-01-03 17:28:07 +0530

    enter google sheet url

--- src/app/apiservices.service.ts
+++ src/app/apiservices.service.ts
@@ -9,7 +9,7 @@ export class ApiservicesService {
   constructor(private http:HttpClient) { }
 
 
-  googleSheetUrl = 'https://script.googleusercontent.com/macros/echo?user_content_key=xVJGAh5usKJv-6sW4YVj14wAlwXITxSZj1XGtCTsvGKy1HbONCp7zbxozCpQl16ksaoxcGG6Jpl2wkg1IdcgxcVMSwGlh4Nym5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnFaL-W0FBYtCa-3uDekW9nB5i559PLmaMILoiPZlxduQ3EqXhD90nWnxME764GKkdJeRuQXWJgvWUU7Z7KErj0epxMBz1zrGBNz9Jw9Md8uu&lib=Mp-KIzC20nSvQm7mC6JAu_woBqQL6oTbp';
+  googleSheetUrl = 'enter_google_sheet_url';
 
   getGoogleSheetValue():Observable<any>{
       return this.http.get(this.googleSheetUrl);
